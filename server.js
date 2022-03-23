ad9182672e4d9a96209e2c88cf0e97f6280fdd39
narendraprasathnp7@gmail.com <narendraprasathnp7@gmail.com>
2022-03-23 16:08:05 +0530

    change port

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ if(process.env.NODE_ENV === 'production'){
   
 
 
-const port = process.env.PORT || 8080
-app.listen(8080, ()=>{
+const port = process.env.PORT || 3000
+app.listen(port, ()=>{
     console.log(
         `Server running on ${process.env.NODE_ENV} mode on port no ${process.env.PORT}`
         .bgMagenta.white
